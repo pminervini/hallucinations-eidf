65c5cf0616ff4f665944a3d11a26a5851ce2f601
Pasquale Minervini <p.minervini@gmail.com>
2023-11-30 01:36:32 +0100

    update

--- main-cli.py
+++ main-cli.py
@@ -26,33 +26,26 @@ def main():
             "apt-get install git-lfs && " \
             "git lfs install && " \
             "git clone https://huggingface.co/spaces/hallucinations-leaderboard/leaderboard && " \
-            "cd leaderboard && pip -U -r requirements.txt && "
-    base_command = "HF_TOKEN=$HF_TOKEN H4_TOKEN=$HF_TOKEN python backend-cli.py"
+            "cd leaderboard && pip -U -r requirements.txt && HF_TOKEN=$HF_TOKEN H4_TOKEN=$HF_TOKEN "
+    command = "python backend-cli.py"
 
     secret_env_vars = configs["env_vars"]
-    commands = {}
-    for config in configs["configs"]:
-        command = base_command + config
-        commands[config.replace("_", "-")] = command
 
-    for run_name, command in commands.items():
-        # Create a Kubernetes Job with a name, container image, and command
-        print(f"Creating job for: {command}")
-        job = KubernetesJob(
-            name=run_name,
-            image=configs["image"],
-            gpu_type="nvidia.com/gpu",
-            gpu_limit=configs["gpu_limit"],
-            gpu_product=configs["gpu_product"],
-            backoff_limit=4,
-            command=["/bin/bash", "-c", "--"],
-            args=[base_args + command],
-            secret_env_vars=secret_env_vars,
-            user_email="p.minervini@ed.ac.uk",
-        )
+    # Create a Kubernetes Job with a name, container image, and command
+    print(f"Creating job for: {command}")
+    job = KubernetesJob(name="hl-backend",
+                        image=configs["image"],
+                        gpu_type="nvidia.com/gpu",
+                        gpu_limit=configs["gpu_limit"],
+                        gpu_product=configs["gpu_product"],
+                        backoff_limit=4,
+                        command=["/bin/bash", "-c", "--"],
+                        args=[base_args + command],
+                        secret_env_vars=secret_env_vars,
+                        user_email="p.minervini@ed.ac.uk")
 
-        # Run the Job on the Kubernetes cluster
-        job.run()
+    # Run the Job on the Kubernetes cluster
+    job.run()
 
 
 if __name__ == "__main__":
