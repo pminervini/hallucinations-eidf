5fbff9afc06bb4cfe27ec3e8f3b2b780c414cc18
Pasquale Minervini <p.minervini@gmail.com>
2023-11-30 10:43:22 +0100

    update

--- main-cli.py
+++ main-cli.py
@@ -58,7 +58,7 @@ def delete_if_completed(job_name: str, namespace: str = 'informatics') -> bool:
             api_res = api.delete_namespaced_job(name=job_name, namespace=namespace,
                                                 body=client.V1DeleteOptions(propagation_policy='Foreground'))
             res = True
-        print(f"Job '{job_name}' deleted. Status: {api_res.status}")
+            print(f"Job '{job_name}' deleted. Status: {api_res.status}")
     return res
 
 def main():
@@ -66,33 +66,34 @@ def main():
     configs = yaml.safe_load(open(args.config, "r"))
 
     job_name = 'hl-backend'
-    delete_if_completed(job_name)
+    not_running = delete_if_completed(job_name)
 
-    base_args = "apt -y update && apt -y upgrade && " \
-            "apt-get -y install git-lfs unzip psmisc wget git python3 python-is-python3 pip bc htop nano && " \
-            "git lfs install && " \
-            "git clone https://huggingface.co/spaces/hallucinations-leaderboard/leaderboard && " \
-            "cd leaderboard && pip install -U -r requirements.txt && pip install -U protobuf && " \
-            "PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python HF_TOKEN=$HF_TOKEN H4_TOKEN=$HF_TOKEN "
-    command = "python backend-cli.py"
+    if not_running is True:
+        base_args = "apt -y update && apt -y upgrade && " \
+                "apt-get -y install git-lfs unzip psmisc wget git python3 python-is-python3 pip bc htop nano && " \
+                "git lfs install && " \
+                "git clone https://huggingface.co/spaces/hallucinations-leaderboard/leaderboard && " \
+                "cd leaderboard && pip install -U -r requirements.txt && pip install -U protobuf && " \
+                "PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python HF_TOKEN=$HF_TOKEN H4_TOKEN=$HF_TOKEN "
+        command = "python backend-cli.py"
 
-    secret_env_vars = configs["env_vars"]
+        secret_env_vars = configs["env_vars"]
 
-    # Create a Kubernetes Job with a name, container image, and command
-    print(f"Creating job for: {command}")
-    job = KubernetesJob(name="hl-backend",
-                        image="nvcr.io/nvidia/cuda:12.0.0-cudnn8-devel-ubuntu22.04",
-                        gpu_type="nvidia.com/gpu",
-                        gpu_limit=configs["gpu_limit"],
-                        gpu_product=configs["gpu_product"],
-                        backoff_limit=1,
-                        command=["/bin/bash", "-c", "--"],
-                        args=[base_args + command],
-                        secret_env_vars=secret_env_vars,
-                        user_email="p.minervini@ed.ac.uk")
+        # Create a Kubernetes Job with a name, container image, and command
+        print(f"Creating job for: {command}")
+        job = KubernetesJob(name="hl-backend",
+                            image="nvcr.io/nvidia/cuda:12.0.0-cudnn8-devel-ubuntu22.04",
+                            gpu_type="nvidia.com/gpu",
+                            gpu_limit=configs["gpu_limit"],
+                            gpu_product=configs["gpu_product"],
+                            backoff_limit=1,
+                            command=["/bin/bash", "-c", "--"],
+                            args=[base_args + command],
+                            secret_env_vars=secret_env_vars,
+                            user_email="p.minervini@ed.ac.uk")
 
-    # Run the Job on the Kubernetes cluster
-    job.run()
+        # Run the Job on the Kubernetes cluster
+        job.run()
 
 
 if __name__ == "__main__":
